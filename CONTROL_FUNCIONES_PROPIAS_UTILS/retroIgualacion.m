61ce7699369a1f5192f1578ab33b4f5c4659e5c1
Julian Cortes <63886922+julian998-dot@users.noreply.github.com>
2022-06-16 15:38:34 -0500

    Update retroIgualacion.m

--- CONTROL_FUNCIONES_PROPIAS_UTILS/retroIgualacion.m
+++ CONTROL_FUNCIONES_PROPIAS_UTILS/retroIgualacion.m
@@ -1,7 +1,7 @@
 function [k]=retroIgualacion(A,B,pol)
 n=length(B);
 syms k1 k2 k3 k4 k5 k6 k7 k8 k9 k10 k11 k12 k13 k14 k15 k16 k17 k18 k19 k20 s;
-kq=[k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14,k15,k16,k17,k18,k19,20];
+kq=[k1,k2,k3,k4,k5,k6,k7,k8,k9,k10,k11,k12,k13,k14,k15,k16,k17,k18,k19,k20];
 for i=1:n
    K(i)=kq(i);
 end
